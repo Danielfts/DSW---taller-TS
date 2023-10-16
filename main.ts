aa00a91817ae02af2070d811d04e959cb76403ab
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2023-10-16 16:00:31 -0500

    Tarea 2

--- main.ts
+++ main.ts
@@ -1,8 +1,50 @@
-console.log("hola mundo!")
+let op1: number = 10;
+let op2: number = 15;
 
-let miTexto:string = "Hola mundo otra vez";
-console.log(miTexto);
+console.log(`Mis variables iniciales son ${op1} y ${op2}`);
 
-console.log(`Esta es la primera linea
-Esta es la segunda linea`);
-console.log(`El valor de mi variable es ${miTexto}`);
+op1++;
+op2 += 4;
+
+console.log(`Mis variables finales son ${op1} ${op2}`);
+
+let resultado:number = op1  / op2;
+console.log(`Resultado: ${resultado}`);
+console.log(`Resultado: ${resultado.toPrecision(2)}`);
+
+// tipos any 
+let varTipoDesconocido:any;
+console.log(`variable tiene valor: ${varTipoDesconocido} y es de tipo ${typeof(varTipoDesconocido)}`)
+
+varTipoDesconocido = "Ahora hay un texto";
+console.log(`variable tiene valor: ${varTipoDesconocido} y es de tipo ${typeof(varTipoDesconocido)}`)
+
+varTipoDesconocido = 2.8;
+console.log(`variable tiene valor: ${varTipoDesconocido} y es de tipo ${typeof(varTipoDesconocido)}`);
+
+let varTipoIndefinido: undefined;
+varTipoIndefinido = undefined;
+
+let varVoid: void = undefined;
+console.log(`variable tien valor: ${varVoid} y es de tipo ${typeof(varVoid)}`);
+
+function imprimirMensaje(): void{
+    console.log("Mensaje genérico");
+}
+
+function imprimirMensaje2(msj:string):void{
+    console.log(msj);
+}
+
+imprimirMensaje();
+imprimirMensaje2("Mensaje por parámetro");
+
+function sumar(op1: number, op2:number):number{
+    return op1 + op2;
+}
+
+var fsumar = sumar;
+
+console.log(sumar(10,15));
+console.log(fsumar(20,25));
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,34 @@
-console.log("hola mundo!");
-var miTexto = "Hola mundo otra vez";
-console.log(miTexto);
-console.log("Esta es la primera linea\nEsta es la segunda linea");
-console.log("El valor de mi variable es ".concat(miTexto));
+var op1 = 10;
+var op2 = 15;
+console.log("Mis variables iniciales son ".concat(op1, " y ").concat(op2));
+op1++;
+op2 += 4;
+console.log("Mis variables finales son ".concat(op1, " ").concat(op2));
+var resultado = op1 / op2;
+console.log("Resultado: ".concat(resultado));
+console.log("Resultado: ".concat(resultado.toPrecision(2)));
+// tipos any 
+var varTipoDesconocido;
+console.log("variable tiene valor: ".concat(varTipoDesconocido, " y es de tipo ").concat(typeof (varTipoDesconocido)));
+varTipoDesconocido = "Ahora hay un texto";
+console.log("variable tiene valor: ".concat(varTipoDesconocido, " y es de tipo ").concat(typeof (varTipoDesconocido)));
+varTipoDesconocido = 2.8;
+console.log("variable tiene valor: ".concat(varTipoDesconocido, " y es de tipo ").concat(typeof (varTipoDesconocido)));
+var varTipoIndefinido;
+varTipoIndefinido = undefined;
+var varVoid = undefined;
+console.log("variable tien valor: ".concat(varVoid, " y es de tipo ").concat(typeof (varVoid)));
+function imprimirMensaje() {
+    console.log("Mensaje genérico");
+}
+function imprimirMensaje2(msj) {
+    console.log(msj);
+}
+imprimirMensaje();
+imprimirMensaje2("Mensaje por parámetro");
+function sumar(op1, op2) {
+    return op1 + op2;
+}
+var fsumar = sumar;
+console.log(sumar(10, 15));
+console.log(fsumar(20, 25));
