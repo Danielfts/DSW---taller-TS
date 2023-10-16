b066e16a68dc1c61796fa9f569b6f3b6daf41682
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2023-10-16 15:44:50 -0500

    first commit

---
+++ main.ts
@@ -0,0 +1,8 @@
+console.log("hola mundo!")
+
+let miTexto:string = "Hola mundo otra vez";
+console.log(miTexto);
+
+console.log(`Esta es la primera linea
+Esta es la segunda linea`);
+console.log(`El valor de mi variable es ${miTexto}`);
---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+console.log("hola mundo!");
+var miTexto = "Hola mundo otra vez";
+console.log(miTexto);
+console.log("Esta es la primera linea\nEsta es la segunda linea");
+console.log("El valor de mi variable es ".concat(miTexto));
