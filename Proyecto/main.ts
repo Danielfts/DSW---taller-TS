778eec5741bae35d8da4b4052a0977e0e34ec7f0
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2023-10-16 16:36:36 -0500

    tarea 3

---
+++ Proyecto/main.ts
@@ -0,0 +1,5 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+
+export const ap = new Aprendiz("Daniel Felipe", "Triviño Santana", "avatar.png", 29, NivelEducativo.UNIVERSITARIO);
+
+console.log(ap);
---
+++ Proyecto/scripts/main.js
@@ -0,0 +1,3 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+export var ap = new Aprendiz("Daniel Felipe", "Triviño Santana", "avatar.png", 29, NivelEducativo.UNIVERSITARIO);
+console.log(ap);
