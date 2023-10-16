f4031a326be68678b4923208d2f69ccb6670e0a5
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2023-10-16 17:07:44 -0500

    Tarea 4

--- Proyecto/main.ts
+++ Proyecto/main.ts
@@ -1,17 +1,45 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+import { Curso } from "./curso.js";
 
-export const ap = new Aprendiz("Daniel Felipe", "Triviño Santana", "avatar.png", 29, NivelEducativo.UNIVERSITARIO);
+let cursos = [
+  new Curso("Practias esenciales para el agilismo", 20, 90, true, 2019),
+  new Curso("Ingeniería de software para la web", 15, 99, true, 2018),
+  new Curso("Pruebas automatizadas", 25, 50, false, 2020),
+  new Curso("Principios de diseño y arquitectura", 20, 75, true, 2020),
+];
 
-let aprendizTable:HTMLElement = document.getElementById("aprendiz")!;
+export const ap = new Aprendiz(
+  "Daniel Felipe",
+  "Triviño Santana",
+  "avatar.png",
+  29,
+  NivelEducativo.UNIVERSITARIO,
+  cursos
+);
+
+console.log(ap.cursos);
+
+let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+
+let estadisticasTable: HTMLElement = document.getElementById("estadisticas")!;
 
 mostrarDatosAprendiz(ap);
+mostrarEstadisticas(ap);
 
-function mostrarDatosAprendiz(aprendiz: Aprendiz):void{
-    let tbodyAprendiz = document.createElement("tbody");
-    tbodyAprendiz.innerHTML = `<tr><td colspan=2><img src="./${aprendiz.avatar}" height:"100"></td></tr>
+function mostrarDatosAprendiz(aprendiz: Aprendiz): void {
+  let tbodyAprendiz = document.createElement("tbody");
+  tbodyAprendiz.innerHTML = `<tr><td colspan=2><img src="./${aprendiz.avatar}" height:"100"></td></tr>
     <tr><td>Nombres:</td><td>${aprendiz.nombres}</td></tr>
     <tr><td>Apellidos:</td><td>${aprendiz.apellidos}</td></tr>
     <tr><td>Nivel educativo:</td><td>${aprendiz.nivelEducativo}</td></tr>
     <tr><td>Edad:</td><td>${aprendiz.edad}</td></tr>`;
-    aprendizTable.appendChild(tbodyAprendiz);
+  aprendizTable.appendChild(tbodyAprendiz);
+}
+
+function mostrarEstadisticas(aprendiz: Aprendiz):void{
+    let numeroCertificados:number = aprendiz.darCursosCertificados();
+    let trelement:HTMLElement = document.createElement("tr")
+    trelement.innerHTML = `<td><b>Cursos certificados</b></td>
+    <td>${numeroCertificados}</td>`;
+    estadisticasTable.appendChild(trelement);
 }
--- Proyecto/scripts/main.js
+++ Proyecto/scripts/main.js
@@ -1,9 +1,25 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
-export var ap = new Aprendiz("Daniel Felipe", "Triviño Santana", "avatar.png", 29, NivelEducativo.UNIVERSITARIO);
+import { Curso } from "./curso.js";
+var cursos = [
+    new Curso("Practias esenciales para el agilismo", 20, 90, true, 2019),
+    new Curso("Ingeniería de software para la web", 15, 99, true, 2018),
+    new Curso("Pruebas automatizadas", 25, 50, false, 2020),
+    new Curso("Principios de diseño y arquitectura", 20, 75, true, 2020),
+];
+export var ap = new Aprendiz("Daniel Felipe", "Triviño Santana", "avatar.png", 29, NivelEducativo.UNIVERSITARIO, cursos);
+console.log(ap.cursos);
 var aprendizTable = document.getElementById("aprendiz");
+var estadisticasTable = document.getElementById("estadisticas");
 mostrarDatosAprendiz(ap);
+mostrarEstadisticas(ap);
 function mostrarDatosAprendiz(aprendiz) {
     var tbodyAprendiz = document.createElement("tbody");
     tbodyAprendiz.innerHTML = "<tr><td colspan=2><img src=\"./".concat(aprendiz.avatar, "\" height:\"100\"></td></tr>\n    <tr><td>Nombres:</td><td>").concat(aprendiz.nombres, "</td></tr>\n    <tr><td>Apellidos:</td><td>").concat(aprendiz.apellidos, "</td></tr>\n    <tr><td>Nivel educativo:</td><td>").concat(aprendiz.nivelEducativo, "</td></tr>\n    <tr><td>Edad:</td><td>").concat(aprendiz.edad, "</td></tr>");
     aprendizTable.appendChild(tbodyAprendiz);
+}
+function mostrarEstadisticas(aprendiz) {
+    var numeroCertificados = aprendiz.darCursosCertificados();
+    var trelement = document.createElement("tr");
+    trelement.innerHTML = "<td><b>Cursos certificados</b></td>\n    <td>".concat(numeroCertificados, "</td>");
+    estadisticasTable.appendChild(trelement);
 }
